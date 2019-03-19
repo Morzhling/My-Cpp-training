0a09f32638a36b5ce6b51af0e7b220b35c2ca211
Morzhling <basiluzumaki@gmail.com>
2019-03-19 21:59:18 +0300

    This is this

---
+++ Указатель This/Указатель This/Указатель This.cpp
@@ -0,0 +1,32 @@
+﻿// Указатель This.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class T {
+private:
+	int a, b, c;
+public:
+	void set(int a, int b, int c){
+		this-> a = a;                //this - ссылка на класс
+		this-> b = b;
+		this-> c = c;
+		}
+	void get() {
+		cout << "A = " << a << " , B = " << b << " , C = " << c << endl;
+	}
+};
+
+int main()
+{
+	T test;
+	test.set(21, 31, 1);
+	test.get();
+
+	return 0;
+
+
+}
+
