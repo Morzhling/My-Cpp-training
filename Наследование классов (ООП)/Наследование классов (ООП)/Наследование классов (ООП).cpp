a69fdfe8c8917479e824b18d9fc1660943a7b1a6
Morzhling <basiluzumaki@gmail.com>
2019-03-20 22:05:53 +0300

    Наследники

---
+++ Наследование классов (ООП)/Наследование классов (ООП)/Наследование классов (ООП).cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+class First {
+protected:
+	int value;
+public:
+	/*void set(int value) {
+		this->value = value;
+	}*/
+	First(int value) {
+		this->value = value;
+		cout << value << endl;
+	}
+};
+
+class Second : public First {				//класс наследник (наследует всё, кроме того что под модификатором private)
+public:
+	Second(int val) : First(val){
+		/*set(val);
+		value = val;
+		cout << value << endl;*/
+	}
+
+};
+
+class Third : public First {				//класс наследник (наследует всё, кроме того что под модификатором private)
+public:
+	Third(int val) : First(val) {
+		//set(val);
+		//value = val;
+		//cout << value << endl;
+	}
+
+};	
+
+
+int main()
+{
+	Second object(213);
+	Third object1(21);
+	return 0;
+}
+
+
