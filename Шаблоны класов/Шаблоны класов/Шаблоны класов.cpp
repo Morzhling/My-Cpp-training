2c977e12c0d647ccf83881265ff9f25cf71a7475
Morzhling <basiluzumaki@gmail.com>
2019-03-22 14:56:51 +0300

    Шаблоны классов

---
+++ Шаблоны класов/Шаблоны класов/Шаблоны класов.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+const int arr_lenght = 3;
+template <class	Type95>
+class Arr {
+private:
+	Type95 arr[arr_lenght];
+public:
+	Arr(Type95* a) {
+		for (int i = 0; i < arr_lenght; i++)
+			arr[i] = a[i];
+
+
+	}
+	Type95 getElement(int i) {
+		return arr[i]; 
+	}
+};
+
+int main()
+{
+	int arr[] = { 12, 41, 11, 6 };
+	Arr <int> obj(arr);
+	cout << obj.getElement(2) << endl;
+
+	char arr1[] = { "Twix!" };
+	Arr <char> obj1(arr1);
+	cout << obj1.getElement(2) << endl;
+	return 0;
+
+}
+
