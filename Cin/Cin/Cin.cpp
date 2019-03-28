e3197df83b320772addf978bb1b964d3805c297c
Morzhling <basiluzumaki@gmail.com>
2019-03-28 10:27:16 +0300

    New

---
+++ Cin/Cin/Cin.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int Var, Var2;
+	cout << "Введите Var: ";
+	cin >> Var; 
+	cout << "Введите Var2: ";
+	cin >> Var2;
+	cout << "Var = " << Var << "\tVar2 = " << Var2;
+}
+
+
---
+++ Операторы/Операторы/Операторы.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{	
+	setlocale(LC_ALL, "rus");
+	int a, b, c;
+	cout << "Введите 3 числа: ";
+	cin >> a >> b >> c;
+	cout << "Сумма чисел = " << a + b + c << endl << "Произведение чисел = " << a * b * c << endl << "Среднее арифметическое чисел = " << (a + b + c) / 3 << endl;
+
+}
+
+
