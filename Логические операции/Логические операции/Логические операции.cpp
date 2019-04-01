52a6f08019eefa86b0fa96bc962ec08fec558add
Morzhling <basiluzumaki@gmail.com>
2019-04-01 21:32:46 +0300

    Logic

---
+++ Логические операции/Логические операции/Логические операции.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 4, b = 5;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
+	cout << (a <= b) << endl;
+	cout << !(a == b) << endl;
+	cout << ((a == b) && (a >= b)) << endl;
+}
+
+
+
