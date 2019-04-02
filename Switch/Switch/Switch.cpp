5d234b21f510c95ed99bd8be5f549312ffc50514
Morzhling <basiluzumaki@gmail.com>
2019-04-02 21:26:15 +0300

    Switch + Calc

---
+++ Switch/Switch/Switch.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	//int a;
+	//cin >> a;
+	//switch (a)
+	//{
+	//case 1:
+	//	cout << "a = 1";
+	//	break;
+	//case 2:
+	//	cout << "a = 2";
+	//	break;
+	//default:
+	//	cout << "wtf!";
+	//	break;
+	//}
+
+	/////////////////////////////////////////////////////									Calculator
+
+	int a, b, i;
+	cout << "Введите код функции, которую хотите выполнить ( 1 - сложение, 2 - вычитание, 3 - умножение, 4 - деление )\n";
+	cin >> a >> b >> i;
+
+	switch (i)
+	{
+	case 1:
+		cout << a + b;
+		break;
+	case 2:
+		cout << a - b;
+		break;
+	case 3:
+		cout << a * b;
+		break;
+	case 4:
+		cout << a / b;
+		break;
+	default:
+		cout << "Введите код от 1 до 4";
+		break;
+	}
+
+}
+
