711947a0ffb8def90d8556d77a74b25b2ff60828
Morzhling <basiluzumaki@gmail.com>
2019-04-02 11:09:29 +0300

    If else if

---
+++ If/If/If.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a;
+	cout << "Введите а, a = ";
+	cin >> a;
+	/*if (a > 5)
+	{
+		cout << "Число больше 5";
+	}
+	else if (a == 5)
+	{
+		cout << "Число равно 5";
+	}
+	else 
+	{
+		cout << "Число меньше 5";
+	}*/
+	if (a % 2 == 0)
+	{
+		cout << "Число а - чётное";
+	}
+	else
+	{
+		cout << "Число а - нечётное";
+	}
+}
+
