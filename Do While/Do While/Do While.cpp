9f3418f63e275f8ab4664622962db1baae51a554
Morzhling <basiluzumaki@gmail.com>
2019-04-03 10:00:27 +0300

    Циклы

---
+++ Do While/Do While/Do While.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int i = 10;
+	do
+	{
+		cout << "i = " << i << endl;				// при "Do While" будет произведен как минимум один заход в цикл
+		i++;
+	} while (i < 10);
+}
---
+++ While/While/While.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int i = 0;
+	while (i<30)
+	{
+		cout << "i = "  << i << endl;				//один проход по телу цикла - одна итерация
+		i++;
+	}
+}
+
