3c92d7e69b7404c0dbc61547c4a6366a8b2e562d
Morzhling <basiluzumaki@gmail.com>
2019-04-04 11:32:47 +0300

    ДЗ

---
+++ ДЗ Do While/ДЗ Do While/ДЗ Do While.cpp
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая выводит сумму всех целых нечетных чисел в заданном пользователем диапазоне
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int Up;
+	int Down;
+	int sum = 0;
+	
+	
+	cout << "Введите диапазон чисел (нижний и верхний)\n";
+	cin >> Down >> Up;
+
+
+
+	do
+	{
+		if (Down % 2 == 1)
+		{
+			sum += Down;
+		}
+			Down++;
+		
+	} 
+	while (Down <= Up);
+	cout << sum;
+}
+
+
---
+++ ДЗ While/ДЗ While/ДЗ While.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int i = 0;
+	char s;
+	bool l = 0;
+	int n = 0;
+
+	cout << "Введите символ" << endl;
+	cin >> s;
+	cout << "Введите количество символов" << endl;
+	cin >> n;
+	cout << "Введите вид линии (0 - верт., 1 - горизонт.)" << endl;
+	cin >> l;
+
+	while (i < n)
+	{
+		if (l == 0)
+		{
+			cout << s << endl;
+			i++;
+		}
+		else
+		{
+			cout << s << " ";
+			i++;
+		}
+	}
+}
