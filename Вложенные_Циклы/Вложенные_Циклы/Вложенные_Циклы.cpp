68675e3a3d83560fb77bcb6ae0248a2dbbbea719
Morzhling <basiluzumaki@gmail.com>
2019-04-15 15:33:56 +0300

    UPD

---
+++ Вложенные_Циклы/Вложенные_Циклы/Вложенные_Циклы.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	for (int i = 1; i < 6; i++)
+	{
+		cout << "Сработал 1ый цикл for итерация № " << i << endl;
+		for (int j = 1; j < 6; j++)
+		{
+			cout << "\tСработал 2oй цикл for итерация №  " << j << endl;
+		}
+
+	}
+
+}
