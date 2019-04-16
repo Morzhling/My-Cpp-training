18b393ddd5c95b7b69c78c15db318b61304ced46
Morzhling <basiluzumaki@gmail.com>
2019-04-16 14:15:22 +0300

    HWCross

---
+++ Вложенные_циклы ДЗ/Вложенные_циклы ДЗ/Вложенные_циклы ДЗ.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	char s = '*';
+	int l;
+	int h;
+
+	cout << "Введите длину l и ширину h прямоуольника:\n";
+	cin >> l >> h;
+
+
+	for (int i = 1; i <= l; i++)
+	{
+		cout << endl << "*";
+		for (int j = 1; j <= h; j++)
+		{
+			cout << "*";
+		}
+
+	}
+
+}
---
+++ Вложенные_циклы ДЗ2/Вложенные_циклы ДЗ2/Вложенные_циклы ДЗ2.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	char s = 'ы';
+	int l;
+	
+
+	cout << "Введите  l креста:\n";
+	cin >> l;
+	int h = l * 3;
+
+	for (int i = 1; i < l; i++)
+	{
+		cout << endl << setw(l+1) << s;
+		for (int j = 1; j < l; j++)
+		{
+			cout << s;
+		}
+
+	}
+
+	for (int i = 1; i < l+1; i++)
+	{
+		cout << endl << s;
+		for (int j = 1; j < h; j++)
+		{
+			cout << s;
+		}
+
+	}
+
+
+	for (int i = 1; i < l; i++)
+	{
+		cout << endl << setw(l+1) << s;
+		for (int j = 1; j < l; j++)
+		{
+			cout << s;
+		}
+
+	}
+
+}
