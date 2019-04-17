5723d220bfb0969ab0e1290a8e8d9cbba8dfa365
Morzhling <basiluzumaki@gmail.com>
2019-04-17 16:50:56 +0300

    Sizeof

---
+++ Sizeof/Sizeof/Sizeof.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+//sizeof возвращает длину в байтах  
+
+int main()
+{
+	int arr[]{12,213,44,23,2342};
+	cout << (sizeof(arr))/4 << endl;
+
+	/*for (int i = 0; i < sizeof(arr) / sizeof(int); i++)*/
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
