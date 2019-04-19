48f4fd571ddbf9f98fbb1d14f9a5c918f38c6150
Morzhling <basiluzumaki@gmail.com>
2019-04-19 15:44:05 +0300

    Func

---
+++ Функции/Функции/Функции.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+void foo()
+{
+	cout << "Я воркен!" << endl;
+}
+
+//int sum(int a, int b)							//С выводом в консоль
+//{
+//	cout << a + b << endl;
+//	return 0;
+//}
+
+//int sum(int a, int b)							//С выводом в консоль и записью в отдельную переменную
+//{
+//	int res = a + b;
+//	cout << res << endl;
+//	return res;
+//	
+//}
+
+//int sum(int a, int b)							//Без вывода в консоль
+//{
+//	int res = a + b;
+//	return res;
+//}
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
+
+void main()								//Точка входа в программу - фунция main
+{
+	setlocale(LC_ALL, "ru");
+	for (int i = 0; i < 10; i++)
+	{
+		foo();
+		int c = sum(324, 23);
+		cout << c << endl;
+	}
+	
+	
+
+}
+
+
