b430ad93a643d9fcfe7bac998e4ce1001475de15
Morzhling <basiluzumaki@gmail.com>
2019-04-24 18:31:13 +0300

    Factorial

---
+++ Факториал (Рекурсия)/Факториал (Рекурсия)/Факториал (Рекурсия).cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+/*Факториал - N! = N*(N-1)!*/
+
+double Factorial(double N)
+{
+	if (N < 0) 
+	{
+		exit(0);
+	}
+	if (N == 0)
+		return 0;
+	if (N == 1)
+		return 1;
+	return N * Factorial(N - 1);
+}
+
+int main()
+{
+	cout << Factorial(6) << endl;
+}
+
