47ac0b5a5b1659d6a8455e5ec0082a5d2e6ef2c1
Morzhling <basiluzumaki@gmail.com>
2019-04-26 14:09:27 +0300

    pArr

---
+++ Указатели и массивы. Арифметика указателей/Указатели и массивы. Арифметика указателей/Указатели и массивы. Арифметика указателей.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	const int SIZE = 10;
+	int arr[SIZE]{21,1231,1213123,3123,31231,5346,8678,67867,3};
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << endl;
+	}
+
+	cout << "===============================================" << endl;
+
+	int *pArr = arr;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << pArr[i] << endl;
+	}
+
+	cout << "===============================================" << endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << *(pArr + i) << endl;
+	}
+
+
+	cout << "arr\t" << arr << endl;
+	cout << "pArr\t" << pArr << endl;
+
+
+
+}
+
+
