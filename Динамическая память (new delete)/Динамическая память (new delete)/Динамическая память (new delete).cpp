fb1e3891385baa705902b40504ca989375ed2bc1
Morzhling <basiluzumaki@gmail.com>
2019-05-05 19:44:05 +0300

    new

---
+++ Динамическая память (new delete)/Динамическая память (new delete)/Динамическая память (new delete).cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+void main()
+{
+	int *pa = new int;
+	*pa = 123;
+	cout << *pa << endl;
+	delete pa;
+	//pa = 0;
+	pa = NULL;
+	//pa = nullptr;
+
+	if (pa != nullptr)
+	{
+		cout << pa << endl;
+	}
+	delete pa;
+
+}
+
