d6100524af60030d02ea37e352e10d9bddd8f160
Morzhling <basiluzumaki@gmail.com>
2019-05-06 15:44:27 +0300

    DArray

---
+++ Динамический массив/Динамический массив/Динамический массив.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int size = 0;
+	srand(time(NULL));
+	cout << "Введите размер массива: " << endl;
+	cin >> size; 
+	cout << "//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////" << endl;
+	int *arr = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			arr[i] = rand() % 100 + 1;
+		}
+		cout << arr[i] << "\t";
+		cout << arr + i << endl;     //Аримфетика указателей (так как массив не разыменован, то  будут выводиться адреса (для значений *(arr+i)))
+	}
+	delete [] arr;
+}
+
