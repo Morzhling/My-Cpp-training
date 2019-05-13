5e8d42a35d8c7e1e596e148c1cdf5fcbf88a2ba5
Morzhling <basiluzumaki@gmail.com>
2019-05-13 15:39:28 +0300

    +

---
+++ Преобразование типов/Преобразование типов/Преобразование типов.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+
+void main()
+{
+	double a = 213.12;
+	cout << a << endl;
+	cout << (int)a << endl;
+	cout << (char)a << endl;			 //явное преобразование
+	cout << (bool)a << endl;
+
+	int z = 231.12;                      //неявное преобразование
+	cout << z << endl;               
+
+
+}
---
+++ Строки, Нуль терминатор/Нуль терминатор/Строки, Нуль терминатор.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+
+
+void main()
+{
+	char symb = 'z';
+	cout << symb << endl;
+	//strlen вернуть размер строки
+	//char symb ="z";     как строка  z\0 не скомпилируется
+	char str[] = "Hello";
+	//char str[] = {'H','e','l','l','o','\0'};
+	cout << str << endl;
+	cout << strlen(str) << endl;
+}
+
