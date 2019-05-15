9e443b8f381e409e386276f86d94193d2b6eb25b
Morzhling <basiluzumaki@gmail.com>
2019-05-15 15:15:13 +0300

    new

---
+++ Конкатенация строк/Конкатенация строк/Конкатенация строк.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	//char res[255]{};
+	//char str1[255] = "Hello";
+	//char str2[255] = "World";
+
+	//cout << str1 << endl;
+
+	////strcat_s(str1, str2);				В Си конкатенация происходит с помощью данной программы, причем в первую строку записывается вторая 
+
+	//strcat_s(res, str1);
+	//strcat_s(res, str2);
+	//cout << res << endl;
+
+	/*string str1 = "Hello";
+	string str2 = "World";
+	string str3 = "!!!";
+	string res;
+
+	res = str1 + " " + str2 + str3 + " HELLYEAH";*/
+
+
+	string str1 = "Текила";
+	string str2 = "Водкина";
+	string str3 = "Боржомовна";
+	//string res;
+
+	//res = "Фамилия: " + str2 + "\tИмя: " + str1 + "\tОтчество: " + str3;
+
+	cout << "Фамилия: " + str2 + "\tИмя: " + str1 + "\tОтчество: " + str3 << endl;
+
+
+}
+
---
+++ Указатель на функцию/Указатель на функцию/Указатель на функцию.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+//тип функции(*имя указателя)(спецификация параметров);
+
+
+//int func1(int a)
+//{
+//	return a - 1;
+//}
+//int func2(int a)
+//{
+//	return a * 2;
+//}
+//
+//void main()
+//{
+//	int(*funcPointer)(int a);
+//	funcPointer = func1;
+//	cout << funcPointer(42);
+//
+//}
+
+
+void ShowInfo(string (*func)())
+{
+	cout << func() << endl;	
+}
+
+string GetDataFromBD()
+{
+	return "Data From BD";
+}
+
+string GetDataFromWebServer()
+{
+	return "Data From WebServer";
+}
+
+void main()
+{
+	ShowInfo(GetDataFromBD);
+}
