379744a4c7a28690e9915c5a415fe910ee2fcb47
Morzhling <basiluzumaki@gmail.com>
2019-05-16 15:05:06 +0300

    Macro

---
+++ Препроцессор. Макрос/Препроцессор. Макрос/Препроцессор. Макрос.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include <iostream>
+#define PI 3.14					//везде в коде до компиляции заменяет PI на 3.14
+#define tab "\t"
+#define begin {
+#define end }
+#define func(x,y)((x)*(y))
+using namespace std;
+
+int main()
+{
+	for (int i = 0; i < 7; i++)
+	begin
+		cout << i << endl;
+	end
+		cout << func(3,12);
+}
