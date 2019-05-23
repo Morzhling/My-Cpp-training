bfd96ddad91c2965848437c5e15e15567c5cc2e2
Morzhling <basiluzumaki@gmail.com>
2019-05-23 11:40:18 +0300

    +

---
+++ Метод класса/Метод класса/Метод класса.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+public:
+	int Age;
+	int Weight;
+	string Name;
+
+	void Info()
+	{
+		/*std :: */cout << "Имя:\t\t" << Name << "\nВес:\t\t" << Weight << "\nВозраст:\t" << Age << endl;
+	}
+};
+
+class Point
+{
+public:
+	int x;
+	int y;
+	int z;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	Point a;
+	a.x = 12;
+	a.y = 21;
+
+	Human Tzar;
+	Tzar.Name = "Basil";
+	Tzar.Age = 24;
+	Tzar.Weight = 68;
+
+	Tzar.Info();
+
+
+	return 0;
+}
